f3f474c6d7d230b07f053c5950692cd91d7a2248
yollosun <yollollay@gmail.com>
2023-03-02 06:22:08 +0600

    initial commit

---
+++ customers/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    is_vip = models.BooleanField(default=False)
+    additional_information = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+    deleted_date = models.DateTimeField(blank=True, null=True)
+
+    # deleted_by = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL,
+    #     on_delete=models.SET_NULL,
+    #     blank=True,
+    #     null=True
+    # )
+
+    def delete(self, using=None, keep_parents=True, deleted_by=None):
+        self.deleted = True
+        self.deleted_date = timezone.now()
+        # self.deleted_by = deleted_by
+        self.save()
+
+    def __str__(self):
+        return self.name
