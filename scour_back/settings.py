ddc4d69d6e2f5c390fe95475771be436cda2046f
yollosun <yollollay@gmail.com>
2023-03-05 23:10:24 +0600

    for digital ocean updates in setting.py

--- scour_back/settings.py
+++ scour_back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ec&7+tb53@0-3d7&1pc_s!ut$updo5nqbue6^61s^g@))z##!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
